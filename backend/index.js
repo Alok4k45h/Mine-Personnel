6fa407edfc00af15b7ed3851dca9e89d7efd9998
Alok kumar <alok4k45h@gmail.com>
2024-09-09 22:59:22 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -18,11 +18,12 @@ mongoose
   .then(console.log("mongodb connected successfully")) //after succussfull connection, displaying successfull message
   .catch((err) => console.log(err)); // after failure, displaying error message
 
+const port = process.env.PORT || 5000;
 // telling app server to use all imported file
 app.use(cors());
 app.use("/backend/user", userRoute);
 
 // app server listening on port 5000 with displaying successful connection message
-app.listen("5000", () => {
+app.listen(port, () => {
   console.log("Server is running on port 5000");
 });
